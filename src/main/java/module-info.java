79df60c1e180ca75ee86a21873e597d7af1dc90c
Fabricio <fjavierbm.jb@gmail.com>
2025-12-08 22:51:43 -0500

    module-info.java fixed for every package pom.xml added graphics depedencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module project.gemclone {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.sql;
 
 
-    opens project.gemclone to javafx.fxml;
+    opens controller to javafx.fxml;
+
     exports project.gemclone;
+    exports controller;
+    exports model;
+    exports ui;
+    exports database;
     }
